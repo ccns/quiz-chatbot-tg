937e76b0b69d92aee1bd2ac58671da0a51288a5c
Yuessiah <yuessiah@gmail.com>
2017-09-01 22:06:15 +0800

    chat id registration

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 from reply import Reply, Judge
 
 url = ""
-entry = {}
+entity = {}
 option_mapping = {}
 
 
@@ -20,73 +20,71 @@ def Reply_markup(have_hint):
     return InlineKeyboardMarkup(keyboard)
 
 
-def Randomize_option(username):
+def Randomize_option(uid):
     global option_mapping
-    om = option_mapping[username] = []
-    op = entry[username]['option']
+    om = option_mapping[uid] = []
+    op = entity[uid]['option']
     while(len(om) != len(op)):
         num = randrange(len(op))
         if num not in om: om.append(num)
 
 
-def Generate_problem(username):
-    global entry
-    entry[username] = requests.get(url+'/question?user='+username).json()
-    Randomize_option(username)
-    op = entry[username]['option']
-    om = option_mapping[username]
+def Generate_problem(uid):
+    global entity
+    entity[uid] = requests.get(url+'/question?user='+uid).json()
+    Randomize_option(uid)
+    op = entity[uid]['option']
+    om = option_mapping[uid]
     for i in range(len(op)):
-        entry[username]['question'] += '\n(' + chr(ord('A')+i) + ') ' + op[om[i]]
+        entity[uid]['question'] += '\n(' + chr(ord('A')+i) + ') ' + op[om[i]]
 
-    return entry[username]['question']
+    return entity[uid]['question']
 
 
 def Start(bot, update):
-    username = update.message.from_user.username
+    chat_id = update.message.chat_id
+    uid = str(chat_id)
 
-    requests.post(url+'/user', json={"user": username})
-    bot.send_message(chat_id=update.message.chat_id, text=Reply('welcome'))
-    bot.send_message(chat_id=update.message.chat_id, text=Generate_problem(username),
-                     reply_markup=Reply_markup(have_hint=(entry[username]['hint'] != '')))
+    requests.post(url+'/user', json={"user": uid, "nickname": update.message.from_user.first_name})
+    bot.send_message(chat_id=chat_id, text=Reply('welcome'))
+    bot.send_message(chat_id=chat_id, text=Generate_problem(uid),
+                     reply_markup=Reply_markup(have_hint=(entity[uid]['hint'] != '')))
 
 
 def Receive_and_reply(bot, update):
     rcv = update.callback_query
-    username = rcv.message.chat.username
     chat_id = rcv.message.chat_id
+    uid = str(chat_id)
 
     if rcv.data == 'hint':
         message_id = rcv.message.message_id
-        reply = entry[username]['question'] + '\nhint: ' + entry[username]['hint']
+        reply = entity[uid]['question'] + '\nhint: ' + entity[uid]['hint']
 
         bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply,
                               reply_markup=Reply_markup(have_hint=False))
     else:
-        id = entry[username]['id']
-        op = entry[username]['option']
-        om = option_mapping[username]
+        id = entity[uid]['id']
+        op = entity[uid]['option']
+        om = option_mapping[uid]
 
-        result = requests.post(url+'/answer', json={'user': username, 'id': id, 'answer': om[int(rcv.data)]}).json()
+        result = requests.post(url+'/answer', json={'user': uid, 'id': id, 'answer': om[int(rcv.data)]}).json()
         bot.send_message(chat_id=chat_id, text=op[om[int(rcv.data)]])
         bot.send_message(chat_id=chat_id, text=Judge(result))
-        bot.send_message(chat_id=chat_id, text=Generate_problem(username),
-                         reply_markup=Reply_markup(have_hint=(entry[username]['hint'] != '')))
+        bot.send_message(chat_id=chat_id, text=Generate_problem(uid),
+                         reply_markup=Reply_markup(have_hint=(entity[uid]['hint'] != '')))
 
 
 def Status(bot, update):
-    username = update.message.from_user.username
-    stat = requests.get(url+'/user?user='+username).json()
+    chat_id = update.message.chat_id
+    uid = str(chat_id)
+    stat = requests.get(url+'/user?user='+uid).json()
     reply = 'Score: ' + str(stat['point']) + '\nRank: ' + str(stat['order']) + '\nRemainders: ' + str(stat['questionStatus'].count(0))
 
-    bot.send_message(chat_id=update.message.chat_id, text=reply)
+    bot.send_message(chat_id=chat_id, text=reply)
 
 
 def Statistic(bot, update):
-    stat = requests.get(url+'/user-database.json').json()
-    reply = 'Total players: ' + str(len(stat))
-    for name in stat:
-        reply += '\n' + name + ', score: ' + str(stat[name]['point'])
-
+    reply = 'http://leaderboard.ccns.ncku.edu.tw'
     bot.send_message(chat_id=update.message.chat_id, text=reply)
 
 
