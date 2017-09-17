931e928acdb94b113a2176a4da3bf6bf82a3c8b7
Yuessiah <yuessiah@gmail.com>
2017-09-17 23:15:40 +0800

    update

--- app.py
+++ app.py
@@ -23,16 +23,18 @@ def Randomize_option(uid):
     shuffle(option_mapping[uid])
 
 
-def Generate_problem(uid):
+def Send_new_problem(bot, chat_id, uid):
     global entity
     entity[uid] = requests.get(url+'/question?user='+uid).json()
     Randomize_option(uid)
     op = entity[uid]['option']
-    om = option_mapping[uid]
-    for i in range(len(op)):
-        entity[uid]['question'] += '\n(' + chr(ord('A')+i) + ') ' + op[om[i]]
 
-    return entity[uid]['question']
+    entity[uid]['question'] = '[' + entity[uid]['category'] + ']\n' + entity[uid]['question']
+    for i in range(len(op)):
+        entity[uid]['question'] += '\n(' + chr(ord('A')+i) + ') ' + op[option_mapping[uid][i]]
+
+    entity[uid]['msg'] = bot.send_message(chat_id=chat_id, text=entity[uid]['question'],
+                                          reply_markup=Reply_markup(have_hint=(entity[uid]['hint'] != '')))
 
 
 def Finish(uid):
@@ -48,51 +50,50 @@ def Finish(uid):
 
 
 def Start(bot, update):
-    global entity, last_msg
+    global entity
     chat_id = update.message.chat_id
     uid = str(chat_id)
 
-    if uid in last_msg:
-        print('Clear board')
-        bot.edit_message_text(chat_id=chat_id, message_id=last_msg[uid].message_id, text=entity[uid]['question'])
+    if uid in entity:
+        bot.edit_message_text(chat_id=chat_id, message_id=entity[uid]['msg'].message_id, text=entity[uid]['question'])
 
-    requests.post(url+'/user', json={'user': uid, 'nickname': update.message.from_user.first_name, 'platform': 'telegram'})
-    bot.send_message(chat_id=chat_id, text=Reply('welcome'))
-    last_msg[uid] = bot.send_message(chat_id=chat_id, text=Generate_problem(uid),
-                     reply_markup=Reply_markup(have_hint=(entity[uid]['hint'] != '')))
+    status = requests.post(url+'/user', json={
+        'user': uid,
+        'nickname': update.message.from_user.first_name,
+        'platform': 'telegram'
+    }).status_code
+
+    if status == 200:
+        bot.send_message(chat_id=chat_id, text=Reply('welcome'))
+
+    Send_new_problem(bot, chat_id, uid)
 
 
 def Receive_and_reply(bot, update):
     rcv = update.callback_query
     chat_id = rcv.message.chat_id
-    uid = str(chat_id)
     message_id = rcv.message.message_id
+    uid = str(chat_id)
+    om = option_mapping[uid]
 
     if rcv.data == 'hint':
         reply = entity[uid]['question'] + '\nHint:\n' + entity[uid]['hint']
-
         bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply,
                               reply_markup=Reply_markup(have_hint=False))
     else:
         bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=entity[uid]['question'])
 
-        id = entity[uid]['id']
-        op = entity[uid]['option']
-        om = option_mapping[uid]
-
-        result = requests.post(url+'/answer', json={'user': uid, 'id': id, 'answer': om[int(rcv.data)]}).json()
-        bot.send_message(chat_id=chat_id, text=op[om[int(rcv.data)]])
+        result = requests.post(url+'/answer', json={'user': uid, 'id': entity[uid]['id'], 'answer': om[int(rcv.data)]}).json()
+        bot.send_message(chat_id=chat_id, text=entity[uid]['option'][om[int(rcv.data)]])
         bot.send_message(chat_id=chat_id, text=Judge(result))
-        if id == 'finish':
-            bot.send_message(chat_id=chat_id, text=Finish(uid))
-
-        last_msg[uid] = bot.send_message(chat_id=chat_id, text=Generate_problem(uid),
-                         reply_markup=Reply_markup(have_hint=(entity[uid]['hint'] != '')))
+        if id == 'finish': bot.send_message(chat_id=chat_id, text=Finish(uid))
+        Send_new_problem(bot, chat_id, uid)
 
 
 def Status(bot, update):
     chat_id = update.message.chat_id
     uid = str(chat_id)
+
     stat = requests.get(url+'/user?user='+uid).json()
     remainders = stat['questionStatus'].count(0)
     reply = 'Score: ' + str(stat['point']) + '\nRank: ' + str(stat['order'])
@@ -104,21 +105,20 @@ def Status(bot, update):
     bot.send_message(chat_id=chat_id, text=reply)
 
 
-def Statistic(bot, update):
+def Leaderboard(bot, update):
     reply = 'http://leaderboard.ccns.ncku.edu.tw'
     bot.send_message(chat_id=update.message.chat_id, text=reply)
 
 
 def Feedback(bot, update):
     reply = 'Submit a new issue:\nhttps://github.com/ccns/quiz-chatbot-tg/issues\nor contact us:\nhttps://www.facebook.com/ncku.ccns'
-    bot.send_message(chat_id=update.message.chat_id, text=reply)
+    bot.send_message(chat_id=update.message.chat_id, text=reply, disable_web_page_preview=True)
 
 
 def main():
-    global entity, option_mapping, url, last_msg
+    global entity, option_mapping, url
 
     entity = {}
-    last_msg = {}
     option_mapping = {}
 
     url = '<url>'
@@ -128,7 +128,7 @@ def main():
     dispatcher.add_handler(CallbackQueryHandler(Receive_and_reply))
     dispatcher.add_handler(CommandHandler('start', Start))
     dispatcher.add_handler(CommandHandler('status', Status))
-    dispatcher.add_handler(CommandHandler('statistic', Statistic))
+    dispatcher.add_handler(CommandHandler('leaderboard', Leaderboard))
     dispatcher.add_handler(CommandHandler('feedback', Feedback))
 
     updater.start_polling()
